a9bac566ffd69d5acf41672a6a7f5fecbf585cc7
rafael Abdalla <rafael.dacostaabdalla@bom.gov.au>
2017-06-02 15:54:30 +1000

    more modifications

--- plotting.py
+++ plotting.py
@@ -20,12 +20,30 @@ avg_gp = pd.to_numeric(df_2017['GP']).mean()
 print('Average Minutes:', avg_ppg)
 print('Average Points per Game:', avg_min)
 print('Games Played:', avg_gp)
+# print(df_2017)
+
+lst = []
+for i in range(2006, 2018):
+    df_list = pd.read_html('http://www.espn.com/nba/team/stats/_/name/det/year/i/seasontype/2', header=0)
+    df_i = df_list[0]
+    df_i.columns = df_i.iloc[0]
+    df_i = df_i.reindex(df_i.index.drop(0))
+    df_i.set_index(['PLAYER'], inplace=True)
+    df_i['YEAR'] = i
+    try:
+        df_total
+    except NameError:
+        df_total = pd.DataFrame()
+    else:
+        df_total = df_total.append(df_i.loc['Totals'])
+        #df_total = df_total.set_index(df_total['YEAR'], drop=True)
+        #df_total = df_total.drop('YEAR', axis=1, inplace=True)
+    df_i.drop('Totals', inplace=True)
+    lst.append(df_i)
 
 
-print(df_2017)
-
-#lst = []
-# for i in range(2006, 2017):
-#    df_list = pd.read_html('http://www.espn.com/nba/team/stats/_/name/det/year/i/seasontype/2', header=0)
-#   df_i = df_list[0]
-#   lst.append(df_i)
+df_total.set_index('YEAR', inplace=True)
+df_total.drop(['GS', 'MIN', 'PER'], axis=1, inplace=True)
+#df_total = df_total.astype(float)
+print('')
+print(df_total)
